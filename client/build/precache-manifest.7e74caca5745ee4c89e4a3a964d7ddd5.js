59d2919164e5965c1f67da5299be49bc74b9b354
franklin,petra <petrafranklin.gmail.com>
2020-10-31 13:31:51 -0700

    initial commit

---
+++ client/build/precache-manifest.7e74caca5745ee4c89e4a3a964d7ddd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdd958bed56adf9196bff304cb89527",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e78a5737ecfd3abdcea",
+    "url": "/static/css/main.acbf1cad.chunk.css"
+  },
+  {
+    "revision": "77107d58c6535e2f4a44",
+    "url": "/static/js/2.6e32f207.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e32f207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e78a5737ecfd3abdcea",
+    "url": "/static/js/main.75ae65d5.chunk.js"
+  },
+  {
+    "revision": "fc6fa6f009dc3f59bcb1",
+    "url": "/static/js/runtime-main.efcc2786.js"
+  }
+]);
---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router, Link } from "@reach/router";
+import Display from "./Components/Display";
+import Registration from "./Components/Registration"
+import One from "./Components/One";
+import New from "./Components/New";
+import Update from "./Components/Update";
+import Visualizer from "./AlgoVisualizer/Visualizer";
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <div>
+          <h1 style={{margin:"40px 0px 0px 0px"}}>An Algorithm Visualizer</h1>
+          <h4>A full stack Javascript Project using Express, React, Mongoose and Node. 
+            </h4>
+            <h4 style={{margin:"20px 0px 0px 0px", color:"grey"}}>by Petra Franklin and Tash Elijah Dutton</h4><br></br>
+        </div>
+        <Router>
+          <Visualizer path="/"/>
+          <Registration path="/new"/>
+          <Display path="/algos/"/>
+          <New path="/algos/:id/newAlgo"/>
+          <Update path="/algos/:id/:aid/update"/>
+          <One path="/algos/:id/:aid"/>
+        </Router>
+        <header className="text-center" style={{margin:"4% 0% 20% 0%", height:"75px", width:"100%"}}>
+          <Link to="/">Visualizer</Link> |&nbsp;
+          <Link to="/algos/">Our Algo Collection</Link>  |&nbsp;
+          <Link to="/algos/5f2445a4784acd0598b11ece/newAlgo">Add an Algo</Link> |&nbsp;
+          <Link to="/pathfinder">Playground</Link> |&nbsp;
+          <Link to="/pathfinder">Playground</Link> |&nbsp; 
+          <Link to="/pathfinder">Playground</Link> |&nbsp;        
+          <Link to="/one/">Random Algo</Link> |&nbsp;
+          <Link to="/new">Login</Link><br></br>
+        </header><br></br>
+      </div>
+    </div>
+  );
+}
+
+export default App;
